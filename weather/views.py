367d3c2d36ba4a9b1f1a36eb3ddf151db3bedb8f
Gumbolit <artemmironkin@gmail.com>
2023-06-15 18:24:53 +0300

    добавил списки городов для админа

--- weather/views.py
+++ weather/views.py
@@ -1,21 +1,28 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 
 # Create your views here.
 def index(request):
     appid='01a00bf0b278ae63c7b0ba81f8599faf'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
-    city="London"
-    res = requests.get(url.format(city)).json()
 
 
-    city_info = {
-        'city': city,
-        'temp': res["main"]["temp"],
-        'icon': res["weather"][0]['icon']
 
-    }
-    context = {'info': city_info}
+    citis = City.objects.all()
+
+    all_cities=[]
+
+    for city in citis:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+         'city': city.name,
+         'temp': res["main"]["temp"],
+         'icon': res["weather"][0]['icon']
+         }
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities}
 
     return render(request, 'weather/index.html', context)
