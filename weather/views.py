6349efcac1160234a874000667d04da5a4075fa0
Gumbolit <artemmironkin@gmail.com>
2023-06-15 15:11:41 +0300

    initial commit

---
+++ weather/views.py
@@ -0,0 +1,20 @@
+import requests
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    appid='01a00bf0b278ae63c7b0ba81f8599faf'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city="London"
+    res = requests.get(url.format(city)).json()
+
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"]
+
+    }
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)
