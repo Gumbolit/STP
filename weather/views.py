8399e6288583cf36c307b9bfa852603a2eb08bcb
Gumbolit <artemmironkin@gmail.com>
2023-06-16 14:49:06 +0300

    Появилась возможность добавить город в список юзеру

--- weather/views.py
+++ weather/views.py
@@ -1,14 +1,19 @@
 import requests
 from django.shortcuts import render
 from .models import City
-
+from .forms import CitiForm
 
 # Create your views here.
 def index(request):
     appid='01a00bf0b278ae63c7b0ba81f8599faf'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
+    if(request.method == 'POST'):
+        form = CitiForm(request.POST)
+        form.save()
 
+
+    form=CitiForm()
 
     citis = City.objects.all()
 
@@ -23,6 +28,6 @@ def index(request):
          }
         all_cities.append(city_info)
 
-    context = {'all_info': all_cities}
+    context = {'all_info': all_cities, 'form':form}
 
     return render(request, 'weather/index.html', context)
